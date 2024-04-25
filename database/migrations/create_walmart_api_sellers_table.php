116c378d3a1b78181e51a952820a798a7f1f5d73
Scott St. John <scottkstjohn87@gmail.com>
2024-04-25 13:39:33 -0700

    Convert class names in migrations to correct casing for project.

--- database/migrations/create_walmart_api_sellers_table.php
+++ database/migrations/create_walmart_api_sellers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class create_walmart_api_sellers_table extends Migration
+class CreateWalmartApiSellersTable extends Migration
 {
     /**
      * Run the migrations.
